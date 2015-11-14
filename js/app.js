3b0d7a989ed5459ca2369e1054af70503aee9366
Ryan Bird <ryan01bird@gmail.com>
2015-11-14 00:48:46 -0700

    added navbar

--- js/app.js
+++ js/app.js
@@ -7,14 +7,6 @@ app.controller('memoriesController', function($scope, $http){
     // var memoriesArray = [];
     $scope.memories = response.data.data
 console.log(response.data.data, '......response......')
-
-    // $scope.old_days = response.data.attributes.old_days
-    // $scope.these_days = response.data.attributes.these_days
-    // $scope.year = response.data.attributes.year
-
-    // $scope.memoriesStuff = function (old_days, these_days, year){
-    //   memoriesArray.push($scope.old_days, $scope.these_days, $scope.year)
-    // }
   })
 })
 
